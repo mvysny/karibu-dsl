8a006890b320d13f9e20751d21bc1202f4066f75
Martin Vysny <mavi@vaadin.com>
2017-06-02 09:11:52 +0300

    Example: initial usage of Vaadin Valo menu

--- example-v8/src/main/kotlin/com/github/vok/karibudsl/example/MyUI.kt
+++ example-v8/src/main/kotlin/com/github/vok/karibudsl/example/MyUI.kt
@@ -5,14 +5,16 @@ import com.github.vok.karibudsl.example.form.FormView
 import com.vaadin.annotations.Theme
 import com.vaadin.annotations.Title
 import com.vaadin.annotations.VaadinServletConfiguration
+import com.vaadin.icons.VaadinIcons
 import com.vaadin.navigator.Navigator
 import com.vaadin.navigator.View
 import com.vaadin.navigator.ViewDisplay
-import com.vaadin.server.VaadinRequest
-import com.vaadin.server.VaadinServlet
+import com.vaadin.server.*
 import com.vaadin.ui.*
 import org.slf4j.bridge.SLF4JBridgeHandler
 import javax.servlet.annotation.WebServlet
+import com.vaadin.ui.themes.ValoTheme
+import com.vaadin.ui.CssLayout
 
 /**
  * The Vaadin UI which demoes all the features. If not familiar with Vaadin, please check out the Vaadin tutorial first.
@@ -22,10 +24,11 @@ import javax.servlet.annotation.WebServlet
 @Title("Karibu-DSL Demo")
 class MyUI : UI() {
 
-    private val content = Content()
+    private val content = ValoMenuLayout()
 
     override fun init(request: VaadinRequest?) {
         setContent(content)
+        Responsive.makeResponsive(this)
         navigator = Navigator(this, content as ViewDisplay)
         navigator.addProvider(autoViewProvider)
     }
@@ -43,26 +46,50 @@ class MyUIServlet : VaadinServlet() {
     }
 }
 
-private class Content: VerticalLayout(), ViewDisplay {
+private class ValoMenuLayout: HorizontalLayout(), ViewDisplay {
+    private val menuArea: CssLayout
     private val viewPlaceholder: CssLayout
-    private lateinit var currentTimeLabel: Label
     init {
         setSizeFull()
-        // the Vaadin DSL demo - build your UI, builder-style!
-        horizontalLayout {
-            w = fillParent
-            menuBar {
-                expandRatio = 1f
-                addItem("Welcome") { WelcomeView.navigateTo() }
-                addItem("Form Demo") { FormView.navigateTo() }
-            }
-            currentTimeLabel = label {
-                setWidthUndefined()
+        isSpacing = false
+
+        menuArea = cssLayout {
+            primaryStyleName = ValoTheme.MENU_ROOT
+            cssLayout {
+                addStyleNames(ValoTheme.MENU_PART, ValoTheme.MENU_PART_LARGE_ICONS)
+                label("Va") {
+                    w = wrapContent; primaryStyleName = ValoTheme.MENU_LOGO
+                }
+                menuButton(VaadinIcons.MENU, "Welcome", "3") {
+                    onLeftClick { WelcomeView.navigateTo() }
+                }
+                menuButton(VaadinIcons.FORM, "Form Demo") {
+                    onLeftClick { FormView.navigateTo() }
+                }
             }
         }
+
         viewPlaceholder = cssLayout {
-            setSizeFull(); expandRatio = 1f
+            primaryStyleName = "valo-content"
+            addStyleName("v-scrollable")
+            setSizeFull()
+            expandRatio = 1f
         }
+    }
+
+    private fun CssLayout.menuButton(icon: Resource, caption: String, badge: String? = null, block: Button.()->Unit) {
+        val b = button {
+            primaryStyleName = ValoTheme.MENU_ITEM
+            this.icon = icon
+            if (badge != null) {
+                isCaptionAsHtml = true
+                this.caption = """$caption <span class="valo-menu-badge">$badge</span>"""
+            } else {
+                this.caption = caption
+            }
+        }
+        // @todo b.addStyleName("selected")
+        b.block()
     }
 
     override fun showView(view: View?) {
