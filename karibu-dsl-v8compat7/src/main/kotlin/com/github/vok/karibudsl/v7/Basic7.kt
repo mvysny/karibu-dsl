0e13ab465849089648c4dcce6f86ba2f6f48ac31
Martin Vysny <mavi@vaadin.com>
2017-06-06 20:21:37 +0300

    minor

--- karibu-dsl-v8compat7/src/main/kotlin/com/github/vok/karibudsl/v7/Basic7.kt
+++ karibu-dsl-v8compat7/src/main/kotlin/com/github/vok/karibudsl/v7/Basic7.kt
@@ -146,7 +146,7 @@ fun (@VaadinDsl AbstractTextField).onEnterPressed(enterListener: (AbstractTextFi
 /**
  * Trims the user input string before storing it into the underlying property data source. Vital for mobile-oriented apps:
  * Android keyboard often adds whitespace to the end of the text when auto-completion occurs. Imagine storing a username ending with a space upon registration:
- * such person can no longer log in from his PC unless he explicitely types in the space.
+ * such person can no longer log in from his PC unless he explicitly types the space character with his keyboard.
  */
 @Deprecated("Deprecated in Vaadin 8")
 fun (@VaadinDsl AbstractField<String>).trimmingConverter() {
