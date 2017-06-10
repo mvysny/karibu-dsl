2944c72a1c65dfddf71bd4f5848b1d4a59a3f043
Martin Vysny <mavi@vaadin.com>
2017-06-10 13:30:12 +0300

    refactoring of Tabsheets

--- example-v8/src/main/kotlin/com/github/vok/karibudsl/example/Tabsheets.kt
+++ example-v8/src/main/kotlin/com/github/vok/karibudsl/example/Tabsheets.kt
@@ -1,6 +1,6 @@
 package com.github.vok.karibudsl.example
 
-import com.github.vok.karibudsl.AutoView
+import com.github.vok.karibudsl.*
 import com.vaadin.data.HasValue
 import com.vaadin.icons.VaadinIcons
 import com.vaadin.navigator.View
@@ -12,68 +12,29 @@ import com.vaadin.ui.themes.ValoTheme
 @AutoView
 class Tabsheets : VerticalLayout(), View {
 
+    private lateinit var closable: CheckBox
+    private lateinit var overflow: CheckBox
+    private lateinit var captions: CheckBox
+    private lateinit var icons: CheckBox
+    private lateinit var disable: CheckBox
+    private lateinit var framed: CheckBox
+    private lateinit var centered: CheckBox
+    private lateinit var rightAlign: CheckBox
+    private lateinit var equal: CheckBox
+    private lateinit var padded: CheckBox
+    private lateinit var compact: CheckBox
+    private lateinit var iconsOnTop: CheckBox
+    private lateinit var selectedOnly: CheckBox
+
     internal var tabs: TabSheet? = null
 
     init {
         isSpacing = false
+        label("Tabs") {
+            addStyleNames(ValoTheme.LABEL_H2, ValoTheme.LABEL_COLORED)
+        }
 
-        val h1 = Label("Tabs")
-        h1.addStyleName(ValoTheme.LABEL_H1)
-        addComponent(h1)
-
-        var wrap = HorizontalLayout()
-        wrap.addStyleName(ValoTheme.LAYOUT_HORIZONTAL_WRAPPING)
-        addComponent(wrap)
-
-        val closable = CheckBox("Closable")
-        wrap.addComponent(closable)
-
-        val overflow = CheckBox("Overflow")
-        wrap.addComponent(overflow)
-
-        val caption = CheckBox("Captions", true)
-        wrap.addComponent(caption)
-
-        val icon = CheckBox("Icons")
-        wrap.addComponent(icon)
-
-        val disable = CheckBox("Disable tabs")
-        wrap.addComponent(disable)
-
-        val h3 = Label("Additional Styles")
-        h3.addStyleName(ValoTheme.LABEL_H3)
-        addComponent(h3)
-
-        wrap = HorizontalLayout()
-        wrap.addStyleName(ValoTheme.LAYOUT_HORIZONTAL_WRAPPING)
-        wrap.margin = MarginInfo(false, false, true, false)
-        addComponent(wrap)
-
-        val framed = CheckBox("Framed", true)
-        wrap.addComponent(framed)
-
-        val centered = CheckBox("Centered tabs")
-        wrap.addComponent(centered)
-
-        val rightAlign = CheckBox("Right-aligned tabs")
-        wrap.addComponent(rightAlign)
-
-        val equal = CheckBox("Equal-width tabs")
-        wrap.addComponent(equal)
-
-        val padded = CheckBox("Padded tabbar")
-        wrap.addComponent(padded)
-
-        val compact = CheckBox("Compact")
-        wrap.addComponent(compact)
-
-        val iconsOnTop = CheckBox("Icons on top")
-        wrap.addComponent(iconsOnTop)
-
-        val selectedOnly = CheckBox("Selected tab closable")
-        wrap.addComponent(selectedOnly)
-
-        val update = { event: HasValue.ValueChangeEvent<Boolean> ->
+        val update = { event: HasValue.ValueChangeEvent<Boolean>? ->
             var style = if (framed.value) "framed " else ""
             style += if (centered.value) " centered-tabs" else ""
             style += if (rightAlign.value) " right-aligned-tabs" else ""
@@ -86,27 +47,64 @@ class Tabsheets : VerticalLayout(), View {
             if (tabs != null) {
                 removeComponent(tabs)
             }
-            tabs = getTabSheet(caption.value!!, style.trim { it <= ' ' },
-                    closable.value!!, overflow.value!!, icon.value!!,
+            tabs = getTabSheet(captions.value!!, style.trim(),
+                    closable.value!!, overflow.value!!, icons.value!!,
                     disable.value!!)
             addComponent(tabs)
         }
-        closable.addValueChangeListener(update)
-        overflow.addValueChangeListener(update)
-        caption.addValueChangeListener(update)
-        icon.addValueChangeListener(update)
-        disable.addValueChangeListener(update)
-        framed.addValueChangeListener(update)
-        centered.addValueChangeListener(update)
-        rightAlign.addValueChangeListener(update)
-        equal.addValueChangeListener(update)
-        padded.addValueChangeListener(update)
-        compact.addValueChangeListener(update)
-        iconsOnTop.addValueChangeListener(update)
-        selectedOnly.addValueChangeListener(update)
+
+        horizontalLayout {
+            styleName = ValoTheme.LAYOUT_HORIZONTAL_WRAPPING
+            closable = checkBox("Closable") {
+                addValueChangeListener(update)
+            }
+            overflow = checkBox("Overflow") {
+                addValueChangeListener(update)
+            }
+            captions = checkBox("Captions", true) {
+                addValueChangeListener(update)
+            }
+            icons = checkBox("Icons") {
+                addValueChangeListener(update)
+            }
+            disable = checkBox("Disable tabs") {
+                addValueChangeListener(update)
+            }
+        }
+        label("Additional Styles") {
+            styleName = ValoTheme.LABEL_H3
+        }
+        horizontalLayout {
+            styleName = ValoTheme.LAYOUT_HORIZONTAL_WRAPPING
+            margin = MarginInfo(false, false, true, false)
+            framed = checkBox("Framed", true) {
+                addValueChangeListener(update)
+            }
+            centered = checkBox("Centered tabs") {
+                addValueChangeListener(update)
+            }
+            rightAlign = checkBox("Right-aligned tabs") {
+                addValueChangeListener(update)
+            }
+            equal = checkBox("Equal-width tabs") {
+                addValueChangeListener(update)
+            }
+            padded = checkBox("Padded tabbar") {
+                addValueChangeListener(update)
+            }
+            compact = checkBox("Compact") {
+                addValueChangeListener(update)
+            }
+            iconsOnTop = checkBox("Icons on top") {
+                addValueChangeListener(update)
+            }
+            selectedOnly = checkBox("Selected tab closable") {
+                addValueChangeListener(update)
+            }
+        }
 
         // Generate initial view
-        icon.value = true
+        update(null)
     }
 
     override fun enter(event: ViewChangeListener.ViewChangeEvent) {
@@ -117,46 +115,36 @@ class Tabsheets : VerticalLayout(), View {
         internal fun getTabSheet(caption: Boolean, style: String, closable: Boolean,
                                  scrolling: Boolean, icon: Boolean, disable: Boolean): TabSheet {
 
-            val ts = TabSheet()
-            ts.addStyleName(style)
-            val sg = StringGenerator.iterator()
+            val ts = TabSheet().apply {
+                styleName = style
+                val sg = StringGenerator.iterator()
 
-            for (i in 1..if (scrolling) 10 else 3) {
-                val tabcaption = if (caption)
-                    sg.next().capitalize() + " " + sg.next()
-                else
-                    null
+                for (i in 1..if (scrolling) 20 else 3) {
+                    verticalLayout {
+                        lastTab.caption = if (caption)
+                            "${sg.next().capitalize()} ${sg.next()}"
+                        else
+                            null
+                        lastTab.isClosable = closable
+                        lastTab.isEnabled = !disable
 
-                val content = VerticalLayout()
-                content.addComponent(Label("Content for tab " + i))
-                if (i == 2) {
-                    content.addComponent(Label(
-                            "Excepteur sint obcaecat cupiditat non proident culpa. Magna pars studiorum, prodita quaerimus."))
-                }
-                val t = ts.addTab(content, tabcaption)
-                t.isClosable = closable
-                t.isEnabled = !disable
+                        // First tab is always enabled
+                        if (i == 1) {
+                            lastTab.isEnabled = true
+                        }
 
-                // First tab is always enabled
-                if (i == 1) {
-                    t.isEnabled = true
-                }
-
-                if (icon) {
-                    t.icon = VaadinIcons.values()[i % VaadinIcons.values().size]
-                }
-            }
-
-            ts.addSelectedTabChangeListener {
-                try {
-                    Thread.sleep(1000)
-                } catch (e: InterruptedException) {
-                    e.printStackTrace()
+                        if (icon) {
+                            lastTab.icon = VaadinIcons.values()[i % VaadinIcons.values().size]
+                        }
+                        label("Content for tab " + i)
+                        if (i == 2) {
+                            label("Excepteur sint obcaecat cupiditat non proident culpa. Magna pars studiorum, prodita quaerimus.")
+                        }
+                    }
+                    addSelectedTabChangeListener { Thread.sleep(500) }
                 }
             }
-
             return ts
         }
     }
-
 }
