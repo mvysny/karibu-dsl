809e76b70ae0da5d66f56d66adaaa5733c7e8f26
Martin Vysny <mavi@vaadin.com>
2017-10-03 15:49:01 +0300

    v10: added proper dsl markers

--- karibu-dsl-v10/src/main/kotlin/com/github/vok/karibudsl/flow/Html.kt
+++ karibu-dsl-v10/src/main/kotlin/com/github/vok/karibudsl/flow/Html.kt
@@ -8,23 +8,23 @@ import com.vaadin.ui.html.*
 
 fun (@VaadinDsl HasComponents).div(block: (@VaadinDsl Div).() -> Unit = {}) = init(Div(), block)
 
-fun HasComponents.h1(text: String, block: (@VaadinDsl H1).() -> Unit = {}) = init(H1(text), block)
-fun HasComponents.h2(text: String, block: (@VaadinDsl H2).() -> Unit = {}) = init(H2(text), block)
-fun HasComponents.h3(text: String, block: (@VaadinDsl H3).() -> Unit = {}) = init(H3(text), block)
-fun HasComponents.h4(text: String, block: (@VaadinDsl H4).() -> Unit = {}) = init(H4(text), block)
-fun HasComponents.h5(text: String, block: (@VaadinDsl H5).() -> Unit = {}) = init(H5(text), block)
-fun HasComponents.h6(text: String, block: (@VaadinDsl H6).() -> Unit = {}) = init(H6(text), block)
-fun HasComponents.hr(block: (@VaadinDsl Hr).() -> Unit = {}) = init(Hr(), block)
+fun (@VaadinDsl HasComponents).h1(text: String, block: (@VaadinDsl H1).() -> Unit = {}) = init(H1(text), block)
+fun (@VaadinDsl HasComponents).h2(text: String, block: (@VaadinDsl H2).() -> Unit = {}) = init(H2(text), block)
+fun (@VaadinDsl HasComponents).h3(text: String, block: (@VaadinDsl H3).() -> Unit = {}) = init(H3(text), block)
+fun (@VaadinDsl HasComponents).h4(text: String, block: (@VaadinDsl H4).() -> Unit = {}) = init(H4(text), block)
+fun (@VaadinDsl HasComponents).h5(text: String, block: (@VaadinDsl H5).() -> Unit = {}) = init(H5(text), block)
+fun (@VaadinDsl HasComponents).h6(text: String, block: (@VaadinDsl H6).() -> Unit = {}) = init(H6(text), block)
+fun (@VaadinDsl HasComponents).hr(block: (@VaadinDsl Hr).() -> Unit = {}) = init(Hr(), block)
 
-fun HasComponents.em(text: String? = null, block: (@VaadinDsl Emphasis).() -> Unit = {}) = init(Emphasis(text), block)
-fun HasComponents.span(text: String? = null, block: (@VaadinDsl Span).() -> Unit = {}) = init(Span(text), block)
-fun HasComponents.anchor(href: String? = null, text: String? = href, block: (@VaadinDsl Anchor).() -> Unit = {}) = init(Anchor(href, text), block)
-fun HasComponents.image(src: String? = null, alt: String? = src, block: (@VaadinDsl Image).() -> Unit = {}) = init(Image(src, alt), block)
-fun HasComponents.label(text: String? = null, `for`: Component? = null, block: (@VaadinDsl Label).() -> Unit = {}) = init(Label(text).apply {
+fun (@VaadinDsl HasComponents).em(text: String? = null, block: (@VaadinDsl Emphasis).() -> Unit = {}) = init(Emphasis(text), block)
+fun (@VaadinDsl HasComponents).span(text: String? = null, block: (@VaadinDsl Span).() -> Unit = {}) = init(Span(text), block)
+fun (@VaadinDsl HasComponents).anchor(href: String? = null, text: String? = href, block: (@VaadinDsl Anchor).() -> Unit = {}) = init(Anchor(href, text), block)
+fun (@VaadinDsl HasComponents).image(src: String? = null, alt: String? = src, block: (@VaadinDsl Image).() -> Unit = {}) = init(Image(src, alt), block)
+fun (@VaadinDsl HasComponents).label(text: String? = null, `for`: Component? = null, block: (@VaadinDsl Label).() -> Unit = {}) = init(Label(text).apply {
     if (`for` != null) setFor(`for`)
 }, block)
 
-fun HasComponents.input(block: (@VaadinDsl Input).() -> Unit = {}) = init(Input(), block)
+fun (@VaadinDsl HasComponents).input(block: (@VaadinDsl Input).() -> Unit = {}) = init(Input(), block)
 // workaround around https://github.com/vaadin/flow/issues/1699
 var Input.placeholder2: String?
 get() = placeholder.orElse(null)
