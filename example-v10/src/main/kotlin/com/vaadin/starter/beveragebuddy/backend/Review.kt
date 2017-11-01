7e5106eec9ca8cd2e8d2e54191cabbeaa562fba7
Martin Vysny <mavi@vaadin.com>
2017-11-01 09:47:39 +0200

    Workaround for issue #3

--- example-v10/src/main/kotlin/com/vaadin/starter/beveragebuddy/backend/Review.kt
+++ example-v10/src/main/kotlin/com/vaadin/starter/beveragebuddy/backend/Review.kt
@@ -8,29 +8,39 @@ import javax.validation.constraints.*
  * Represents a beverage review.
  */
 // must be open - Flow requires it to create ModelProxy
-open class Review(open var id: Long? = null,
-                  
-                  @field:NotNull
-                  @field:Min(1)
-                  @field:Max(5)
-                  var score: Int = 1,
+// must have a zero-arg constructor: workaround for https://github.com/mvysny/karibu-dsl/issues/3
+open class Review : Serializable {
+    open var id: Long? = null
 
-                  @field:NotBlank
-                  @field:Size(min = 3)
-                  var name: String = "",
+    @field:NotNull
+    @field:Min(1)
+    @field:Max(5)
+    var score: Int = 1
 
-                  @field:NotNull
-                  @field:PastOrPresent
-                  var date: LocalDate = LocalDate.now(),
+    @field:NotBlank
+    @field:Size(min = 3)
+    var name: String = ""
 
-                  @field:NotNull
-                  var category: Category = Category.UNDEFINED,
+    @field:NotNull
+    @field:PastOrPresent
+    var date: LocalDate = LocalDate.now()
 
-                  @field:NotNull
-                  @field:Min(1)
-                  @field:Max(99)
-                  var count: Int = 1) : Serializable {
+    @field:NotNull
+    var category: Category = Category.UNDEFINED
+
+    @field:NotNull
+    @field:Min(1)
+    @field:Max(99)
+    var count: Int = 1
+
     override fun toString() = "Review(id=$id, score=$score, name='$name', date=$date, category=$category, count=$count)"
 
-    fun copy() = Review(id, score, name, date, category.copy(), count)
+    fun copy() = Review().also {
+        it.id = id
+        it.score = score
+        it.name = name
+        it.date = date
+        it.category = category.copy()
+        it.count = count
+    }
 }
