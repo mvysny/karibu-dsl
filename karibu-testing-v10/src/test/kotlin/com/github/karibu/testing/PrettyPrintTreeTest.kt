d80f5684629c13619f4985baccb320427daa1aee
Martin Vysny <martin.vysny@ericsson.com>
2018-02-26 12:11:05 +0200

    Added more tests for PrettyPrintTree

--- karibu-testing-v10/src/test/kotlin/com/github/karibu/testing/PrettyPrintTreeTest.kt
+++ karibu-testing-v10/src/test/kotlin/com/github/karibu/testing/PrettyPrintTreeTest.kt
@@ -1,7 +1,11 @@
 package com.github.karibu.testing
 
 import com.github.mvysny.dynatest.DynaTest
+import com.vaadin.flow.component.Text
+import com.vaadin.flow.component.button.Button
 import com.vaadin.flow.component.html.Div
+import com.vaadin.flow.component.textfield.TextArea
+import com.vaadin.flow.component.textfield.TextField
 import kotlin.test.expect
 
 class PrettyPrintTreeTest : DynaTest({
@@ -12,5 +16,13 @@ class PrettyPrintTreeTest : DynaTest({
         expect("""└── Div[text='Foo']
     └── Text[text='Foo']
 """) { div.toPrettyTree() }
+    }
+
+    test("toPrettyString()") {
+        expect("Text[text='foo']") { Text("foo").toPrettyString() }
+        expect("Div[INVIS]") { Div().apply { isVisible = false } .toPrettyString() }
+        expect("TextField[#25, value='']") { TextField().apply { id_ = "25" } .toPrettyString() }
+        expect("Button[text='click me']") { Button("click me").toPrettyString() }
+        expect("TextArea[label='label', value='some text']") { TextArea("label").apply { value = "some text" } .toPrettyString() }
     }
 })
