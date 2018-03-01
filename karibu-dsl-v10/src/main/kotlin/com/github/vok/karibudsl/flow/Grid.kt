3a011caad9d95a41f339fac6659a42ae2c33bc53
Martin Vysny <mavi@vaadin.com>
2018-03-01 16:55:26 +0200

    improved kdoc

--- karibu-dsl-v10/src/main/kotlin/com/github/vok/karibudsl/flow/Grid.kt
+++ karibu-dsl-v10/src/main/kotlin/com/github/vok/karibudsl/flow/Grid.kt
@@ -7,8 +7,10 @@ import kotlin.reflect.KProperty1
 fun <T : Any?> (@VaadinDsl HasComponents).grid(block: (@VaadinDsl Grid<T>).() -> Unit = {}) = init(Grid(), block)
 
 /**
- * Adds a column for given [property]. The column key is set to the property name; the column is also by default set to sortable
+ * Adds a column for given [property]. The column key is set to the property name, so that you can look up the column
+ * using [getColumnBy]. The column is also by default set to sortable
  * unless the [sortable] parameter is set otherwise.
+ * @param block runs given block on the column.
  * @return the newly created column
  */
 fun <T, V : Comparable<V>> Grid<T>.addColumnFor(property: KProperty1<T, V?>, sortable: Boolean = true, block: Grid.Column<T>.() -> Unit = {}): Grid.Column<T> =
@@ -21,8 +23,10 @@ fun <T, V : Comparable<V>> Grid<T>.addColumnFor(property: KProperty1<T, V?>, sor
     }
 
 /**
- * Adds a column for given [property]. The column key is set to the property name; the column is also by default set to sortable
+ * Adds a column for given [property]. The column key is set to the property name, so that you can look up the column
+ * using [getColumnBy]. The column is also by default set to sortable
  * unless the [sortable] parameter is set otherwise.
+ * @param block runs given block on the column.
  * @return the newly created column
  */
 @JvmName("addColumnFor2")
@@ -42,7 +46,7 @@ fun <T, V : Comparable<V>> Grid<T>.addColumnFor(property: KProperty1<T, V>, sort
  * Example of usage:
  * ```
  * grid<Person> {
- *     addColumn(Person::name).apply {
+ *     addColumn({ it.name }).apply {
  *         setHeader("Name")
  *         sortProperty = Person::name
  *     }
@@ -55,3 +59,10 @@ var <T> Grid.Column<T>.sortProperty: KProperty1<T, *>
     set(value) {
         setSortProperty(value.name)
     }
+
+/**
+ * Retrieves the column for given [property]; it matches [Grid.Column.getKey] to [KProperty1.name].
+ * @throws IllegalArgumentException if no such column exists.
+ */
+fun <T> Grid<T>.getColumnBy(property: KProperty1<T, *>): Grid.Column<T> =
+    getColumnByKey(property.name) ?: throw IllegalArgumentException("No column with key $property; available column keys: ${columns.map { it.key } .filterNotNull()}")
