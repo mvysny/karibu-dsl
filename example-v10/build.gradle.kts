2668157929f130a0a2153b531f0cd74e50ccb4a4
Martin Vysny <martin@vysny.me>
2018-10-18 12:59:15 +0300

    Gradle: initial conversion to Kotlin

---
+++ example-v10/build.gradle.kts
@@ -0,0 +1,31 @@
+// The Beverage Buddy sample project ported to Kotlin.
+// Original project: https://github.com/vaadin/beverage-starter-flow
+
+plugins {
+    id("io.spring.dependency-management") version "1.0.6.RELEASE"
+    id("org.gretty")
+    war
+}
+
+gretty {
+    contextPath = "/"
+    servletContainer = "jetty9.4"
+}
+
+dependencyManagement {
+    imports { mavenBom("com.vaadin:vaadin-bom:${ext["vaadin10_version"]}") }
+}
+
+dependencies {
+    compile(project(":karibu-dsl-v10"))
+    providedCompile("javax.servlet:javax.servlet-api:3.1.0")
+
+    // logging
+    // currently we are logging through the SLF4J API to LogBack. See src/main/resources/logback.xml file for the logger configuration
+    compile("ch.qos.logback:logback-classic:1.2.3")
+    compile("org.slf4j:slf4j-api:1.7.25")
+
+    testCompile("com.github.kaributesting:karibu-testing-v10:${ext["kaributesting_version"]}")
+    testCompile("com.github.mvysny.dynatest:dynatest-engine:${ext["dynatest_version"]}")
+    testCompile("org.jetbrains.kotlin:kotlin-test")
+}
