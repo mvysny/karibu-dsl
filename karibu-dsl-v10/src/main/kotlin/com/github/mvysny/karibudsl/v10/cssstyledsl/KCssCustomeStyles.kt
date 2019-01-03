9db3366b5d16b38fe4eccc33686c3b087d52f26c
Vitalii Lipovetskii <vlipovetskii@users.noreply.github.com>
2019-01-03 18:31:36 +0200

    Delete KCssCustomeStyles.kt

--- karibu-dsl-v10/src/main/kotlin/com/github/mvysny/karibudsl/v10/cssstyledsl/KCssCustomeStyles.kt
+++
@@ -1,7 +0,0 @@
-package com.github.vok.karibudsl.flow.cssstyledsl
-
-import com.github.vok.karibudsl.flow.cssstyledsl.properties.color
-
-val cssBlueColorStyle: KCssStyleBlock = {
-    color = KCssColor.Blue.value
-}
