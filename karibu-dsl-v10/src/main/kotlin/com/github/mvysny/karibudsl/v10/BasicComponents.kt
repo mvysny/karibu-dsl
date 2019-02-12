7677a4ca19704b4afe705d4bfb719a2329e46d04
Martin Vysny <martin@vysny.me>
2019-02-12 08:41:09 +0200

    fixed kdoc

--- karibu-dsl-v10/src/main/kotlin/com/github/mvysny/karibudsl/v10/BasicComponents.kt
+++ karibu-dsl-v10/src/main/kotlin/com/github/mvysny/karibudsl/v10/BasicComponents.kt
@@ -16,8 +16,8 @@ fun Component.fireEvent(event: ComponentEvent<*>) {
 }
 
 /**
- * The [com.vaadin.flow.component.button.Button.click] function sends the click request to the browser, which will then send the click event
- * to the server. This function avoids the roundtrip and works even for browserless testing.
+ * Adds [com.vaadin.flow.component.button.Button.click] functionality to all [ClickNotifier]s. This function directly calls
+ * all click listeners, thus it avoids the roundtrip to client and back. It even works with browserless testing.
  */
 fun <R> R.serverClick() where R : Component, R : ClickNotifier<R> {
     fireEvent(ClickEvent<R>(this, true, -1, -1, -1, -1, 1, -1, false, false, false, false))
