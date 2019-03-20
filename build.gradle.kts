8f3c0a7a490c32920eafab6184007317f2ce787c
Martin Vysny <martin@vysny.me>
2019-03-20 15:02:21 +0200

    0.6.4-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "com.github.mvysny.karibudsl"
-    version = "0.6.3"
+    version = "0.6.4-SNAPSHOT"
 
     repositories {
         mavenCentral()
