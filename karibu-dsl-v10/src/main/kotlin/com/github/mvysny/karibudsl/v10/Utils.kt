93110b5e710a0fb2df5c1521bc3be0831bf0c84c
Martin Vysny <martin@vysny.me>
2019-08-28 09:49:46 +0300

    Vaadin 10+: Add support for context menu listener

--- karibu-dsl-v10/src/main/kotlin/com/github/mvysny/karibudsl/v10/Utils.kt
+++ karibu-dsl-v10/src/main/kotlin/com/github/mvysny/karibudsl/v10/Utils.kt
@@ -1,5 +1,9 @@
 package com.github.mvysny.karibudsl.v10
 
+import com.vaadin.flow.component.Component
+import com.vaadin.flow.dom.DomEventListener
+import com.vaadin.flow.dom.DomListenerRegistration
+
 fun String.containsWhitespace(): Boolean = any { it.isWhitespace() }
 
 private val messages = mapOf("cantConvertToInteger" to "Can't convert to integer",
@@ -14,3 +18,19 @@ private val messages = mapOf("cantConvertToInteger" to "Can't convert to integer
  * Change this function to provide a proper i18n for your apps. For a list of all keys used by Karibu-DSL see [messages].
  */
 var karibuDslI18n: (key: String) -> String = { key -> messages[key] ?: key }
+
+/**
+ * Adds the right-click (context-menu) [listener] to the component. Also causes the right-click browser
+ * menu not to be shown on this component (see [preventDefault]).
+ */
+fun Component.addContextMenuListener(listener: DomEventListener): DomListenerRegistration =
+        element.addEventListener("contextmenu", listener)
+                .preventDefault()
+
+/**
+ * Makes the client-side listener call [Event.preventDefault()](https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault)
+ * on the event.
+ *
+ * @return this
+ */
+fun DomListenerRegistration.preventDefault(): DomListenerRegistration = addEventData("event.preventDefault()")
