a9ffabe34ad106003b394f61b0f87dc66d91fcbd
Martin Vysny <mavi@vaadin.com>
2019-12-20 21:01:13 +0200

    0.7.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "com.github.mvysny.karibudsl"
-    version = "0.7.4-SNAPSHOT"
+    version = "0.7.4"
 
     repositories {
         mavenCentral()
