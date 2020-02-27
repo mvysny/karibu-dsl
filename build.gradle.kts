4a2271a88e592b7c29a1d14a4123e42b3d9e733f
Martin Vysny <mavi@vaadin.com>
2020-02-27 08:15:14 +0200

    v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ import java.util.*
 
 plugins {
     kotlin("jvm") version "1.3.61"
-    id("org.gretty") version "2.3.1"
+    id("org.gretty") version "3.0.1"
     id("com.jfrog.bintray") version "1.8.3"
     `maven-publish`
     id("org.jetbrains.dokka") version "0.9.17"
