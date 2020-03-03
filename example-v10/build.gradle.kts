6f9e1e0706ea1a146b92b35d17bbff9b502b8217
Martin Vysny <mavi@vaadin.com>
2020-03-03 13:53:33 +0200

    minor

--- example-v10/build.gradle.kts
+++ example-v10/build.gradle.kts
@@ -13,9 +13,9 @@ gretty {
 }
 
 dependencies {
-    compile(project(":karibu-dsl-v10"))
+    implementation(project(":karibu-dsl-v10"))
     // Vaadin 14
-    compile("com.vaadin:vaadin-core:${properties["vaadin10_version"]}") {
+    implementation("com.vaadin:vaadin-core:${properties["vaadin10_version"]}") {
         // Webjars are only needed when running in Vaadin 13 compatibility mode
         listOf("com.vaadin.webjar", "org.webjars.bowergithub.insites",
                 "org.webjars.bowergithub.polymer", "org.webjars.bowergithub.polymerelements",
@@ -23,9 +23,9 @@ dependencies {
                 .forEach { exclude(group = it) }
     }
     providedCompile("javax.servlet:javax.servlet-api:3.1.0")
-    compile("org.slf4j:slf4j-simple:${properties["slf4j_version"]}")
+    implementation("org.slf4j:slf4j-simple:${properties["slf4j_version"]}")
 
-    testCompile("com.github.mvysny.kaributesting:karibu-testing-v10:${properties["kaributesting_version"]}")
-    testCompile("com.github.mvysny.dynatest:dynatest-engine:${properties["dynatest_version"]}")
-    testCompile("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("com.github.mvysny.kaributesting:karibu-testing-v10:${properties["kaributesting_version"]}")
+    testImplementation("com.github.mvysny.dynatest:dynatest-engine:${properties["dynatest_version"]}")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
--- example-v8/build.gradle.kts
+++ example-v8/build.gradle.kts
@@ -19,21 +19,21 @@ gretty {
 val staging by configurations.creating
 
 dependencies {
-    compile(project(":karibu-dsl-v8"))
+    implementation(project(":karibu-dsl-v8"))
 
     // logging
     // currently we are logging through the SLF4J API to slf4j-simple. See src/main/resources/simplelogger.properties file for the logger configuration
-    compile("org.slf4j:slf4j-simple:${properties["slf4j_version"]}")
-    compile("org.slf4j:slf4j-api:${properties["slf4j_version"]}")
+    implementation("org.slf4j:slf4j-simple:${properties["slf4j_version"]}")
+    implementation("org.slf4j:slf4j-api:${properties["slf4j_version"]}")
     // this will allow us to configure Vaadin to log to SLF4J
-    compile("org.slf4j:jul-to-slf4j:${properties["slf4j_version"]}")
+    implementation("org.slf4j:jul-to-slf4j:${properties["slf4j_version"]}")
 
     // Vaadin: workaround until https://youtrack.jetbrains.com/issue/IDEA-178071 is fixed
-    compile("com.vaadin:vaadin-client-compiled:${properties["vaadin8_version"]}")
-    compile("com.vaadin:vaadin-themes:${properties["vaadin8_version"]}")
+    implementation("com.vaadin:vaadin-client-compiled:${properties["vaadin8_version"]}")
+    implementation("com.vaadin:vaadin-themes:${properties["vaadin8_version"]}")
 
-    testCompile("com.github.mvysny.kaributesting:karibu-testing-v8:${properties["kaributesting_version"]}")
-    testCompile("com.github.mvysny.dynatest:dynatest-engine:${properties["dynatest_version"]}")
+    testImplementation("com.github.mvysny.kaributesting:karibu-testing-v8:${properties["kaributesting_version"]}")
+    testImplementation("com.github.mvysny.dynatest:dynatest-engine:${properties["dynatest_version"]}")
 
     // heroku app runner
     staging("com.github.jsimone:webapp-runner-main:9.0.27.1")
