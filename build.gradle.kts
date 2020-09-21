686b1bc238907179e8eb2d98515a1d4c17190782
Martin Vysny <mavi@vaadin.com>
2020-09-21 08:51:17 +0300

    1.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "com.github.mvysny.karibudsl"
-    version = "1.0.3-SNAPSHOT"
+    version = "1.0.3"
 
     repositories {
         jcenter()
