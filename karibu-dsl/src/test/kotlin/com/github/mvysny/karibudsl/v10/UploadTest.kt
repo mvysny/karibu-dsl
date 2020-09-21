a1f6653dd9f3a1a9cc48b49171bff04c402f0523
Martin Vysny <mavi@vaadin.com>
2020-09-21 08:32:26 +0300

    Add support for Upload. Closes #26

---
+++ karibu-dsl/src/test/kotlin/com/github/mvysny/karibudsl/v10/UploadTest.kt
@@ -0,0 +1,22 @@
+package com.github.mvysny.karibudsl.v10
+
+import com.github.mvysny.dynatest.DynaTest
+import com.github.mvysny.kaributesting.v10.MockVaadin
+import com.github.mvysny.kaributesting.v10._expectOne
+import com.vaadin.flow.component.UI
+import com.vaadin.flow.component.upload.Receiver
+import com.vaadin.flow.component.upload.Upload
+import java.io.ByteArrayOutputStream
+
+class UploadTest : DynaTest({
+    beforeEach { MockVaadin.setup() }
+    afterEach { MockVaadin.tearDown() }
+
+    test("dsl") {
+        UI.getCurrent().upload {  }
+        _expectOne<Upload>()
+        UI.getCurrent().removeAll()
+        UI.getCurrent().upload(Receiver { fileName, mimeType -> ByteArrayOutputStream() })
+        _expectOne<Upload>()
+    }
+})
