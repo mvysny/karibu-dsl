2771e0fba9cbfff6552670fcf70510c366c2d2a1
Martin Vysny <mavi@vaadin.com>
2020-11-16 13:01:27 +0200

    Implement Dialog.center()

--- karibu-dsl/src/main/kotlin/com/github/mvysny/karibudsl/v10/Dialogs.kt
+++ karibu-dsl/src/main/kotlin/com/github/mvysny/karibudsl/v10/Dialogs.kt
@@ -22,3 +22,18 @@ public fun getAllDialogs(): List<Dialog> = UI.getCurrent().getAllDialogs()
  * Returns all dialogs opened in the given UI.
  */
 public fun UI.getAllDialogs(): List<Dialog> = children.toList().filterIsInstance<Dialog>()
+
+/**
+ * Centers the dialog on-screen.
+ */
+public fun Dialog.center() {
+    // https://github.com/vaadin/vaadin-dialog/issues/220
+    val styles: MutableList<String> = mutableListOf<String>()
+    if (width != null) {
+        styles.add("width: $width")
+    }
+    if (height != null) {
+        styles.add("height: $height")
+    }
+    element.executeJs("this.$.overlay.$.overlay.style = '${styles.joinToString(";")}';")
+}
