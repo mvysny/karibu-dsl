8b9a284930a423ce67c44575363c5caf4b07674e
Martin Vysny <mavi@vaadin.com>
2020-12-21 16:17:57 +0200

    1.0.5-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "com.github.mvysny.karibudsl"
-    version = "1.0.4"
+    version = "1.0.5-SNAPSHOT"
 
     repositories {
         jcenter()
