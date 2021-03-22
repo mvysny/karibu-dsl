68887beb2de55983036fc557f5c62879bb996423
Martin Vysny <mavi@vaadin.com>
2021-03-22 09:57:40 +0200

    Removed tests for Vaadin 18, added tests for Vaadin 20

--- karibu-dsl/karibu-dsl-testrun-vaadin19/src/test/kotlin/com/github/mvysny/karibudsl/v19/AllTest.kt
+++ karibu-dsl/karibu-dsl-testrun-vaadin19/src/test/kotlin/com/github/mvysny/karibudsl/v19/AllTest.kt
@@ -1,6 +1,9 @@
-package com.github.mvysny.karibudsl.v10
+package com.github.mvysny.karibudsl.v19
 
 import com.github.mvysny.dynatest.DynaTest
+import com.github.mvysny.karibudsl.v10.allTests
+import com.github.mvysny.karibudsl.v10.loadAsProperties
+import com.github.mvysny.karibudsl.v10.vaadinVersion
 import com.github.mvysny.kaributesting.v10.VaadinMeta
 import java.io.File
 import java.util.*
--- karibu-dsl/karibu-dsl-testrun-vaadin20/src/test/kotlin/com/github/mvysny/karibudsl/v20/AllTest.kt
+++ karibu-dsl/karibu-dsl-testrun-vaadin20/src/test/kotlin/com/github/mvysny/karibudsl/v20/AllTest.kt
@@ -1,6 +1,9 @@
-package com.github.mvysny.karibudsl.v10
+package com.github.mvysny.karibudsl.v20
 
 import com.github.mvysny.dynatest.DynaTest
+import com.github.mvysny.karibudsl.v10.allTests
+import com.github.mvysny.karibudsl.v10.loadAsProperties
+import com.github.mvysny.karibudsl.v10.vaadinVersion
 import com.github.mvysny.kaributesting.v10.VaadinMeta
 import java.io.File
 import java.util.*
@@ -8,12 +11,12 @@ import kotlin.test.expect
 
 class AllTest : DynaTest({
     test("vaadin version") {
-        expect(18) { VaadinMeta.version }
+        expect(20) { VaadinMeta.version }
     }
 
     test("vaadin version 2") {
         val gradleProps: Properties = File("../../gradle.properties").loadAsProperties()
-        val expectedVaadinVersion: String = gradleProps["vaadin18_version"] as String
+        val expectedVaadinVersion: String = gradleProps["vaadin20_version"] as String
         expect(expectedVaadinVersion) { vaadinVersion }
     }
 
