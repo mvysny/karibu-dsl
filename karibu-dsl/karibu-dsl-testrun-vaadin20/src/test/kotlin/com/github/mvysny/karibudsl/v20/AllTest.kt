a5865005949bb00e72d67e1dfab1384fefe17c8d
Martin Vysny <mavi@vaadin.com>
2021-03-22 09:59:53 +0200

    fixed tests

--- karibu-dsl/karibu-dsl-testrun-vaadin20/src/test/kotlin/com/github/mvysny/karibudsl/v20/AllTest.kt
+++ karibu-dsl/karibu-dsl-testrun-vaadin20/src/test/kotlin/com/github/mvysny/karibudsl/v20/AllTest.kt
@@ -4,6 +4,7 @@ import com.github.mvysny.dynatest.DynaTest
 import com.github.mvysny.karibudsl.v10.allTests
 import com.github.mvysny.karibudsl.v10.loadAsProperties
 import com.github.mvysny.karibudsl.v10.vaadinVersion
+import com.github.mvysny.kaributesting.v10.SemanticVersion
 import com.github.mvysny.kaributesting.v10.VaadinMeta
 import java.io.File
 import java.util.*
@@ -16,8 +17,8 @@ class AllTest : DynaTest({
 
     test("vaadin version 2") {
         val gradleProps: Properties = File("../../gradle.properties").loadAsProperties()
-        val expectedVaadinVersion: String = gradleProps["vaadin20_version"] as String
-        expect(expectedVaadinVersion) { vaadinVersion }
+        val expectedVaadinVersion: SemanticVersion = SemanticVersion.fromString(gradleProps["vaadin20_version"] as String)
+        expect(expectedVaadinVersion) { VaadinMeta.fullVersion }
     }
 
     allTests()
