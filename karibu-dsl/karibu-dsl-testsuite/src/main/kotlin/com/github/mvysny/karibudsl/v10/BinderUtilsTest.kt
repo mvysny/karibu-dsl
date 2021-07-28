b72d67de189006cf3160da1ae5dbaa03767f3073
Martin Vysny <mavi@vaadin.com>
2021-07-28 15:51:35 +0300

    minor

--- karibu-dsl/karibu-dsl-testsuite/src/main/kotlin/com/github/mvysny/karibudsl/v10/BinderUtilsTest.kt
+++ karibu-dsl/karibu-dsl-testsuite/src/main/kotlin/com/github/mvysny/karibudsl/v10/BinderUtilsTest.kt
@@ -1,7 +1,6 @@
 package com.github.mvysny.karibudsl.v10
 
 import com.github.mvysny.dynatest.DynaNodeGroup
-import com.github.mvysny.dynatest.DynaTest
 import com.github.mvysny.kaributesting.v10.MockVaadin
 import com.github.mvysny.kaributesting.v10._value
 import com.vaadin.flow.component.AbstractField
