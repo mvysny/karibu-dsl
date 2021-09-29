5e6e841b71efd94866e4aa912b829a9c2842b1d8
Martin Vysny <mavi@vaadin.com>
2021-09-29 09:51:55 +0300

    Add DSL for UploadI18n

--- karibu-dsl/karibu-dsl-testsuite/src/main/kotlin/com/github/mvysny/karibudsl/v10/UploadTest.kt
+++ karibu-dsl/karibu-dsl-testsuite/src/main/kotlin/com/github/mvysny/karibudsl/v10/UploadTest.kt
@@ -1,13 +1,13 @@
 package com.github.mvysny.karibudsl.v10
 
 import com.github.mvysny.dynatest.DynaNodeGroup
-import com.github.mvysny.dynatest.DynaTest
 import com.github.mvysny.kaributesting.v10.MockVaadin
 import com.github.mvysny.kaributesting.v10._expectOne
 import com.vaadin.flow.component.UI
 import com.vaadin.flow.component.upload.Receiver
 import com.vaadin.flow.component.upload.Upload
 import java.io.ByteArrayOutputStream
+import kotlin.test.expect
 
 fun DynaNodeGroup.uploadTest() {
     beforeEach { MockVaadin.setup() }
@@ -19,5 +19,38 @@ fun DynaNodeGroup.uploadTest() {
         UI.getCurrent().removeAll()
         UI.getCurrent().upload(Receiver { _, _ -> ByteArrayOutputStream() })
         _expectOne<Upload>()
+    }
+
+    test("i18n dsl") {
+        val upload = UI.getCurrent().upload {
+            i18n {
+                dropFiles {
+                    many = "Foo"
+                }
+                addFiles {
+                    one = "Bar"
+                }
+                error {
+                    fileIsTooBig = "Baz"
+                }
+                uploading {
+                    status {
+                        connecting = "Foo connecting"
+                    }
+                    remainingTime {
+                        prefix = "Bar prefix"
+                    }
+                    error {
+                        forbidden = "Baz forbidden"
+                    }
+                }
+            }
+        }
+        expect("Foo") { upload.i18n.dropFiles.many }
+        expect("Bar") { upload.i18n.addFiles.one }
+        expect("Baz") { upload.i18n.error.fileIsTooBig }
+        expect("Foo connecting") { upload.i18n.uploading.status.connecting }
+        expect("Bar prefix") { upload.i18n.uploading.remainingTime.prefix }
+        expect("Baz forbidden") { upload.i18n.uploading.error.forbidden }
     }
 }
--- karibu-dsl/src/main/kotlin/com/github/mvysny/karibudsl/v10/Upload.kt
+++ karibu-dsl/src/main/kotlin/com/github/mvysny/karibudsl/v10/Upload.kt
@@ -3,7 +3,64 @@ package com.github.mvysny.karibudsl.v10
 import com.vaadin.flow.component.HasComponents
 import com.vaadin.flow.component.upload.Receiver
 import com.vaadin.flow.component.upload.Upload
+import com.vaadin.flow.component.upload.UploadI18N
 
 @VaadinDsl
 public fun (@VaadinDsl HasComponents).upload(receiver: Receiver? = null, block: (@VaadinDsl Upload).() -> Unit = {}): Upload
         = init(Upload(receiver), block)
+
+@VaadinDsl
+public fun (@VaadinDsl Upload).i18n(block: (@VaadinDsl UploadI18N).() -> Unit): UploadI18N {
+    if (i18n == null) i18n = UploadI18N()
+    i18n.block()
+    return i18n
+}
+
+@VaadinDsl
+public fun (@VaadinDsl UploadI18N).dropFiles(block: (@VaadinDsl UploadI18N.DropFiles).() -> Unit): UploadI18N.DropFiles {
+    if (dropFiles == null) dropFiles = UploadI18N.DropFiles()
+    dropFiles.block()
+    return dropFiles
+}
+
+@VaadinDsl
+public fun (@VaadinDsl UploadI18N).addFiles(block: (@VaadinDsl UploadI18N.AddFiles).() -> Unit): UploadI18N.AddFiles {
+    if (addFiles == null) addFiles = UploadI18N.AddFiles()
+    addFiles.block()
+    return addFiles
+}
+
+@VaadinDsl
+public fun (@VaadinDsl UploadI18N).error(block: (@VaadinDsl UploadI18N.Error).() -> Unit): UploadI18N.Error {
+    if (error == null) error = UploadI18N.Error()
+    error.block()
+    return error
+}
+
+@VaadinDsl
+public fun (@VaadinDsl UploadI18N).uploading(block: (@VaadinDsl UploadI18N.Uploading).() -> Unit): UploadI18N.Uploading {
+    if (uploading == null) uploading = UploadI18N.Uploading()
+    uploading.block()
+    return uploading
+}
+
+@VaadinDsl
+public fun (@VaadinDsl UploadI18N.Uploading).status(block: (@VaadinDsl UploadI18N.Uploading.Status).() -> Unit): UploadI18N.Uploading.Status {
+    if (status == null) status = UploadI18N.Uploading.Status()
+    status.block()
+    return status
+}
+
+@VaadinDsl
+public fun (@VaadinDsl UploadI18N.Uploading).remainingTime(block: (@VaadinDsl UploadI18N.Uploading.RemainingTime).() -> Unit): UploadI18N.Uploading.RemainingTime {
+    if (remainingTime == null) remainingTime = UploadI18N.Uploading.RemainingTime()
+    remainingTime.block()
+    return remainingTime
+}
+
+@VaadinDsl
+public fun (@VaadinDsl UploadI18N.Uploading).error(block: (@VaadinDsl UploadI18N.Uploading.Error).() -> Unit): UploadI18N.Uploading.Error {
+    if (error == null) error = UploadI18N.Uploading.Error()
+    error.block()
+    return error
+}
