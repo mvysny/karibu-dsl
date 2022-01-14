a0febe48314215d088b0c0914a2d7a1843ca0bec
Martin Vysny <mavi@vaadin.com>
2022-01-14 10:41:50 +0200

    minor

--- karibu-dsl/karibu-dsl-testsuite/src/main/kotlin/com/github/mvysny/karibudsl/v10/VaadinComponentsTest.kt
+++ karibu-dsl/karibu-dsl-testsuite/src/main/kotlin/com/github/mvysny/karibudsl/v10/VaadinComponentsTest.kt
@@ -2,11 +2,9 @@ package com.github.mvysny.karibudsl.v10
 
 import com.github.mvysny.dynatest.DynaNodeGroup
 import com.github.mvysny.kaributesting.v10.*
-import com.github.mvysny.dynatest.DynaTest
 import com.github.mvysny.dynatest.DynaTestDsl
 import com.vaadin.flow.component.Text
 import com.vaadin.flow.component.UI
-import com.vaadin.flow.component.html.Div
 import com.vaadin.flow.component.html.Span
 import com.vaadin.flow.component.icon.VaadinIcon
 import com.vaadin.flow.component.orderedlayout.FlexLayout
