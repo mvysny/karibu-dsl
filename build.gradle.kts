ef440a1579a660302f56b508d9872fdd9458368e
Martin Vysny <mavi@vaadin.com>
2023-03-06 13:21:11 +0200

    2.0.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "com.github.mvysny.karibudsl"
-    version = "2.0.0"
+    version = "2.0.1-SNAPSHOT"
 
     repositories {
         maven { setUrl("https://maven.vaadin.com/vaadin-prereleases") }  // remove when Vaadin 24 is released
