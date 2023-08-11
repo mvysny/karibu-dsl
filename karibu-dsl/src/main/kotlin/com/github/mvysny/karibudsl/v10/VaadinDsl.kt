fcf4d0ff67adc5ac81dceafdc5c66050166c2342
Martin Vysny <mavi@vaadin.com>
2023-08-11 12:52:27 +0300

    VaadinDsl.init(): document further

--- karibu-dsl/src/main/kotlin/com/github/mvysny/karibudsl/v10/VaadinDsl.kt
+++ karibu-dsl/src/main/kotlin/com/github/mvysny/karibudsl/v10/VaadinDsl.kt
@@ -12,9 +12,22 @@ public annotation class VaadinDsl
 /**
  * When introducing extensions for your custom components, just call [init] in your extension function. For example:
  *
- * `fun HasComponents.shinyComponent(caption: String? = null, block: ShinyComponent.()->Unit = {}) = init(ShinyComponent(caption), block)`
+ * ```kotlin
+ * fun HasComponents.shinyComponent(caption: String? = null, block: ShinyComponent.()->Unit = {}) =
+ *   init(ShinyComponent(caption), block)
+ * ```
  *
- * Adds [component] to receiver, see [HasComponents.add] for details.
+ * Adding the [VaadinDsl] annotation makes the function appear in different color in Intellij, and also
+ * prevents you from calling a DSL function from a grandparent, but makes the function more complicated:
+ *
+ * ```kotlin
+ * @VaadinDsl
+ * fun (@VaadinDsl HasComponents).shinyComponent(caption: String? = null, block: (@VaadinDsl ShinyComponent).()->Unit = {}) =
+ *   init(ShinyComponent(caption), block)
+ * ```
+ *
+ * Adds [component] to `receiver` via [HasComponents.add] and runs [block] on the component,
+ * allowing you to configure the component further.
  *
  * @param component the component to attach
  * @param block optional block to run over the component, allowing you to add children to the [component]
@@ -23,7 +36,7 @@ public annotation class VaadinDsl
 public fun <T : Component> (@VaadinDsl HasComponents).init(
     component: T,
     id: String?,
-    block: T.() -> Unit = {}
+    block: (@VaadinDsl T).() -> Unit = {}
 ): T {
     if (id != null) {
         component.setId(id)
