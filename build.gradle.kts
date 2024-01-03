bacf36c82992b8d0b78c6a25e01a20f92e8da6dd
Martin Vysny <mavi@vaadin.com>
2024-01-04 00:34:19 +0100

    kotlin v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.9.20"
+    kotlin("jvm") version "1.9.22"
     id("com.vaadin") apply(false)
     `maven-publish`
     signing
