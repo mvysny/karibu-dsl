eca66db54a80f1b89b01a9e2125061aebddb52b6
Vitalii Lipovetskii <vlipovetskii@users.noreply.github.com>
2024-12-11 18:50:04 +0200

    Migration all the functions like fun (@VaadinDsl HasComponents).routerLink(icon: VaadinIcon?, ... from VaadinIcon to IconFactory fun (@VaadinDsl HasComponents).routerLink(icon: IconFactory?, ..

--- karibu-dsl/src/main/kotlin/com/github/mvysny/karibudsl/v10/Router.kt
+++ karibu-dsl/src/main/kotlin/com/github/mvysny/karibudsl/v10/Router.kt
@@ -2,17 +2,17 @@ package com.github.mvysny.karibudsl.v10
 
 import com.vaadin.flow.component.Component
 import com.vaadin.flow.component.HasComponents
-import com.vaadin.flow.component.icon.VaadinIcon
+import com.vaadin.flow.component.icon.IconFactory
 import com.vaadin.flow.router.HasUrlParameter
 import com.vaadin.flow.router.RouterLink
 import kotlin.reflect.KClass
 import com.vaadin.flow.router.RouteParameters
 
 @VaadinDsl
-public fun (@VaadinDsl HasComponents).routerLink(icon: VaadinIcon? = null, text: String? = null, viewType: KClass<out Component>,
+public fun (@VaadinDsl HasComponents).routerLink(icon: IconFactory? = null, text: String? = null, viewType: KClass<out Component>,
                                                  block: (@VaadinDsl RouterLink).() -> Unit = {}): RouterLink {
     val link = RouterLink(null as String?, viewType.java)
-    if (icon != null) link.icon(icon)
+    if (icon != null) link.init(icon.create())
     if (text != null) link.text(text)
     init(link, block)
     return link
@@ -20,10 +20,10 @@ public fun (@VaadinDsl HasComponents).routerLink(icon: VaadinIcon? = null, text:
 
 @JvmName("routerLinkWithParams")
 @VaadinDsl
-public fun (@VaadinDsl HasComponents).routerLink(icon: VaadinIcon? = null, text: String? = null, viewType: KClass<out Component>,
+public fun (@VaadinDsl HasComponents).routerLink(icon: IconFactory? = null, text: String? = null, viewType: KClass<out Component>,
                                                         parameters: Map<String, String>, block: (@VaadinDsl RouterLink).() -> Unit = {}): RouterLink {
     val link = RouterLink(null as String?, viewType.java, RouteParameters(parameters))
-    if (icon != null) link.icon(icon)
+    if (icon != null) link.init(icon.create())
     if (text != null) link.text(text)
     init(link, block)
     return link
@@ -31,10 +31,10 @@ public fun (@VaadinDsl HasComponents).routerLink(icon: VaadinIcon? = null, text:
 
 @JvmName("routerLinkWithParam")
 @VaadinDsl
-public fun <T, C> (@VaadinDsl HasComponents).routerLink(icon: VaadinIcon? = null, text: String? = null, viewType: KClass<out C>,
+public fun <T, C> (@VaadinDsl HasComponents).routerLink(icon: IconFactory? = null, text: String? = null, viewType: KClass<out C>,
                                                         parameter: T, block: (@VaadinDsl RouterLink).() -> Unit = {}): RouterLink where C: Component, C: HasUrlParameter<T> {
     val link = RouterLink(null as String?, viewType.java, parameter)
-    if (icon != null) link.icon(icon)
+    if (icon != null) link.init(icon.create())
     if (text != null) link.text(text)
     init(link, block)
     return link
@@ -47,10 +47,10 @@ public fun <T, C> (@VaadinDsl HasComponents).routerLink(icon: VaadinIcon? = null
 // routerLink(null, "Edit", EditArticleView::class, article.id)    // no need for Long, compiler will infer it.
 
 @VaadinDsl
-public fun (@VaadinDsl HasComponents).routerLink(icon: VaadinIcon? = null, text: String? = null, block: (@VaadinDsl RouterLink).() -> Unit = {}): RouterLink {
+public fun (@VaadinDsl HasComponents).routerLink(icon: IconFactory? = null, text: String? = null, block: (@VaadinDsl RouterLink).() -> Unit = {}): RouterLink {
     // a RouterLink for which the navigation target is not yet known and is set lazily, perhaps in HasUrlParameter.setParameter()
     val link = RouterLink()
-    if (icon != null) link.icon(icon)
+    if (icon != null) link.init(icon.create())
     if (text != null) link.text(text)
     init(link, block)
     return link
