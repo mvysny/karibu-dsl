be95b74fe5acfd3a971327f024856b4cc279604c
Martin Vysny <mavi@vaadin.com>
2024-12-12 07:59:56 +0200

    icon{}: now accepts IconFactory
    (cherry picked from commit d9e7b65b792fdbca3c510cbb467e74c13cfcc84c)

--- karibu-dsl/src/main/kotlin/com/github/mvysny/karibudsl/v10/Icon.kt
+++ karibu-dsl/src/main/kotlin/com/github/mvysny/karibudsl/v10/Icon.kt
@@ -2,6 +2,7 @@ package com.github.mvysny.karibudsl.v10
 
 import com.vaadin.flow.component.HasComponents
 import com.vaadin.flow.component.icon.Icon
+import com.vaadin.flow.component.icon.IconFactory
 import com.vaadin.flow.component.icon.VaadinIcon
 
 /**
@@ -9,8 +10,8 @@ import com.vaadin.flow.component.icon.VaadinIcon
  * of possible alternative themes for the button.
  */
 @VaadinDsl
-public fun (@VaadinDsl HasComponents).icon(icon: VaadinIcon, block: (@VaadinDsl Icon).() -> Unit = {}): Icon =
-        init(Icon(icon), block)
+public fun (@VaadinDsl HasComponents).icon(icon: IconFactory, block: (@VaadinDsl Icon).() -> Unit = {}): Icon =
+        init(icon.create(), block)
 
 @VaadinDsl
 public fun (@VaadinDsl HasComponents).icon(collection: String, icon: String, block: (@VaadinDsl Icon).() -> Unit = {}): Icon =
