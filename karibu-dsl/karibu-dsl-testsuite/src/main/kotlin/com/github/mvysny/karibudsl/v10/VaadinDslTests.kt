2623968e27e9f010b274ce774f12dcc8024c6eef
Martin Vysny <mavi@vaadin.com>
2025-02-26 13:42:30 +0200

    Add tests for VaadinDsl

--- karibu-dsl/karibu-dsl-testsuite/src/main/kotlin/com/github/mvysny/karibudsl/v10/VaadinDslTests.kt
+++ karibu-dsl/karibu-dsl-testsuite/src/main/kotlin/com/github/mvysny/karibudsl/v10/VaadinDslTests.kt
@@ -1,12 +1,45 @@
 package com.github.mvysny.karibudsl.v10
 
+import com.github.mvysny.kaributesting.v10.MockVaadin
+import com.github.mvysny.kaributesting.v10._expectOne
+import com.vaadin.flow.component.UI
+import com.vaadin.flow.component.button.Button
 import com.vaadin.flow.component.orderedlayout.VerticalLayout
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import kotlin.test.expect
 
 abstract class AbstractVaadinDslTests {
+    @BeforeEach fun setup() { MockVaadin.setup() }
+    @AfterEach fun tearDown() { MockVaadin.tearDown() }
+
+    @Nested inner class initTests {
+        @Test fun smoke() {
+            UI.getCurrent().init(Button("Hi"), "hi") {}
+            UI.getCurrent().init(Button("Hi"), "hi")
+            UI.getCurrent().init(Button("Hi")) {}
+            UI.getCurrent().init(Button("Hi"))
+        }
+
+        @Test fun initLambdaCalled() {
+            var called = false
+            UI.getCurrent().init(Button("Hi"), "hi") { called = true }
+            expect(true) { called }
+        }
+
+        @Test fun componentAddedToParent() {
+            val b = Button("Hi")
+            UI.getCurrent().init(b, "hi")
+            _expectOne<Button> { id = "hi" }
+            _expectOne<Button> { text = "Hi" }
+            expect(b) { UI.getCurrent().children.toList().single() }
+            expect(UI.getCurrent()) { b.parent.get() }
+        }
+    }
+
     @Nested
     inner class provideSingleComponentOrNull {
         @Test
