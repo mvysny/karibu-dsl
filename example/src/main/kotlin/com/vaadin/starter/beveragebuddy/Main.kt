e7455914328d3f2fccc37623daddb996b21b3bf7
Martin Vysny <mavi@vaadin.com>
2026-01-11 15:57:50 +0200

    port theming to Vaadin 25 way

--- example/src/main/kotlin/com/vaadin/starter/beveragebuddy/Main.kt
+++ example/src/main/kotlin/com/vaadin/starter/beveragebuddy/Main.kt
@@ -1,15 +1,16 @@
 package com.vaadin.starter.beveragebuddy
 
 import com.github.mvysny.vaadinboot.VaadinBoot
-import com.vaadin.flow.component.dependency.CssImport
+import com.vaadin.flow.component.dependency.StyleSheet
 import com.vaadin.flow.component.page.AppShellConfigurator
 import com.vaadin.flow.component.page.BodySize
 import com.vaadin.flow.component.page.Viewport
-import com.vaadin.flow.theme.Theme
+import com.vaadin.flow.theme.lumo.Lumo
 
 @BodySize(width = "100vw", height = "100vh")
-@Theme("my-theme")
 @Viewport("width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes")
+@StyleSheet(Lumo.STYLESHEET)
+@StyleSheet("styles.css")
 class AppShell: AppShellConfigurator
 
 /**
