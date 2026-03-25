e9c659edd003b3f6e0106280fb9bd9abd8b31af9
Martin Vysny <mavi@vaadin.com>
2026-03-25 15:50:25 +0200

    2.6.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "com.github.mvysny.karibudsl"
-    version = "2.6.0"
+    version = "2.6.1-SNAPSHOT"
 
     repositories {
         mavenCentral()
